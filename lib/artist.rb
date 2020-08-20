cf91b8e7ec5803d995dbae2a1368420b0acda377
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 14:05:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
 
-  attr_accessor :name, :songs
+  attr_accessor :name
+  attr_reader :songs 
 
   @@song_count = 0
 
@@ -10,16 +11,20 @@ class Artist
   end
 
   def add_song(song)
+    @@song_count += 1
     @songs << song
-    #song.artist = self
+    song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    self.add_song(song)
+    song
   end
+  
+  def song_count
+    self.songs.size 
+  end 
 
   def self.song_count
     @@song_count
