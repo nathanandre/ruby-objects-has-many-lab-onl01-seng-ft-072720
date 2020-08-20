39864f2bddf58e60054432225b3c209bb89a7245
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 15:45:50 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,34 @@
 class Artist
 
-  attr_accessor :name, :songs
+  attr_accessor :name
+  #attr_reader :songs 
 
-  @@song_count = 0
+  @@song_count = 1
 
   def initialize(name)
     @name = name
     @songs = []
   end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
 
   def add_song(song)
+    @@song_count += 1
     @songs << song
-    #song.artist = self
+    song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    self.add_song(song)
+    song
   end
+  
+  def song_count
+    self.songs.size 
+  end 
 
   def self.song_count
     @@song_count
--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,37 @@
 class Author
 
-  attr_accessor :name, :posts
+  attr_accessor :name
+  #attr_reader :songs 
 
-  @@post_count = 0
-  
+  @@post_count = 1
+
   def initialize(name)
     @name = name
     @posts = []
   end
+  
+  def posts 
+    Post.all.select {|post| post.author == self}
+  end 
 
   def add_post(post)
+    @@post_count += 1
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    self.add_post(post)
+    post 
   end
+  
+  def post_count
+    self.posts.size 
+  end 
 
   def self.post_count
     @@post_count
   end
+
 end
