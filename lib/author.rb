82e6bb88bbfbf47eceb3b42870d0393960f217fd
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 15:20:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,37 @@
 class Author
 
-  attr_accessor :name, :posts
+  attr_accessor :name
+  #attr_reader :songs 
 
-  @@post_count = 0
-  
+  @@song_count = 1
+
   def initialize(name)
     @name = name
-    @posts = []
+    @songs = []
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+
+  def add_song(song)
+    @@song_count += 1
+    @songs << song
+    song.artist = self
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.add_song(song)
+    song
+  end
+  
+  def song_count
+    self.songs.size 
+  end 
+
+  def self.song_count
+    @@song_count
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
-    @@post_count
-  end
 end
